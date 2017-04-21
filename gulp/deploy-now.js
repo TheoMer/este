afdc7e0c005748a1822682aba4af19733ade3747
TheoMer <6e240757@use.startmail.com>
2017-04-21 09:39:54 +0100

    Changes

--- gulp/deploy-now.js
+++ gulp/deploy-now.js
@@ -3,7 +3,7 @@ import spawn from 'cross-spawn';
 import gulp from 'gulp';
 
 gulp.task('deploy-now', ['deploy-firebase-database'], done => {
-  const sha = childProcess.execSync('git rev-parse HEAD').toString().trim();
+  const sha = spawn.childProcess.execSync('git rev-parse HEAD').toString().trim();
   spawn('now', ['-e', 'NODE_ENV=production', '-e', `appVersion=${sha}`], {
     stdio: 'inherit',
   })
